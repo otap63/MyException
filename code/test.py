5d68adfb6b1f797910b5264022db792974c1f839
otap63 <otap63@gmail.com>
2020-11-21 23:15:57 -0500

    source files are restructured under directory code.

---
+++ code/test.py
@@ -0,0 +1,23 @@
+def main(mylist):
+    name = choice(mylist)
+    l2 = [dict(one='1'), ]
+
+
+mfun()
+
+
+class Person():
+    def __init__(self, name):
+        self.greeting = 'Hello{name}'
+        self.name = name
+
+    def __str__(self):
+        return self.make_greeting()
+
+    def make_greeting(self):
+        return self.greeting.format(name=self.name)
+
+
+if __name__ == '__main__':
+    { for  in }
+    p = {{'1': 'one'}}
